bda43c3e6507ad5273b22822ddb65bf53b8d53cc
Youssef Nashed <youssef.shady@gmail.com>
2021-12-14 09:13:08 -0600

    SDF submit job script changes

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=alphafold
 #SBATCH --output=output-%j.txt --error=output-%j.txt
 #SBATCH --nodes=1
-#SBATCH --gpus=a100:1
+#SBATCH --gpus=a100:2
 #SBATCH --mem=131072
 
 DBS_PATH=/sdf/group/ml/CryoNet/alphafold/databases/
@@ -15,7 +15,8 @@ FASTA_PATH=/sdf/group/ml/CryoNet/alphafold/test_fasta/T1050.fasta
 #mkdir -p "${LSCRATCH_PATH}"
 #cp -aruv "${DBS_PATH}/." "${LSCRATCH_PATH}"
 
-singularity run --env TF_FORCE_UNIFIED_MEMORY=1,XLA_PYTHON_CLIENT_MEM_FRACTION=4.0,OPENMM_CPU_THREADS=${SLURM_NTASKS} \
+singularity run --env TF_FORCE_UNIFIED_MEMORY=1,XLA_PYTHON_CLIENT_MEM_FRACTION=4.0 \
+                --env OPENMM_CPU_THREADS=${SLURM_NTASKS},NVIDIA_VISIBLE_DEVICES=all \
                 -B /sdf -B ${DBS_PATH}:/data -B .:/etc --nv ${SIF_PATH} \
                 --n_cpu=${SLURM_NTASKS} \
                 --fasta_paths=${FASTA_PATH} \
@@ -26,8 +27,11 @@ singularity run --env TF_FORCE_UNIFIED_MEMORY=1,XLA_PYTHON_CLIENT_MEM_FRACTION=4
                 --pdb70_database_path=/data/pdb70/pdb70 \
                 --template_mmcif_dir=/data/pdb_mmcif/mmcif_files \
                 --obsolete_pdbs_path=/data/pdb_mmcif/obsolete.dat \
-                --bfd_database_path=/data/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
-                --uniclust30_database_path=/data/uniclust30/uniclust30_2018_08/uniclust30_2018_08 \
+                --small_bfd_database_path=/data/small_bfd/bfd-first_non_consensus_sequences.fasta \
                 --max_template_date=2021-07-28 \
                 --model_preset=monomer \
-                --db_preset=full_dbs
+                --db_preset=reduced_dbs
+
+# To run with --db_preset=full_dbs
+#                --uniclust30_database_path=/data/uniclust30/uniclust30_2018_08/uniclust30_2018_08 \
+#                --bfd_database_path=/data/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt 
